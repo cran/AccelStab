9d5536a73f7036ab60fdbbb82ca662e4f4e0c8e5
Alex Ball <alex_keith_ball@hotmail.com>
2023-12-10 11:20:08 +0000

    version 1.0.0

---
+++ inst/doc/Introduction.R
@@ -0,0 +1,31 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----echo=FALSE, fig.align='center'-------------------------------------------
+
+knitr::include_graphics("img/arrhenius.jpg")
+
+
+## ----echo=FALSE, fig.align='center'-------------------------------------------
+
+knitr::include_graphics("img/arrhenius_simplied.jpg")
+
+
+## ----echo=FALSE, fig.align='center'-------------------------------------------
+
+knitr::include_graphics("img/degradation.jpg")
+
+
+## ----echo=FALSE, fig.align='center'-------------------------------------------
+
+knitr::include_graphics("img/response_var.jpg")
+
+
+## ----echo=FALSE, out.width='700px', out.height='200px', fig.align="center"----
+
+knitr::include_graphics("img/parameterisation.jpg")
+
+
